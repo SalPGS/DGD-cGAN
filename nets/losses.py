fd9f2b670d53021aa283bf4d8eb158755c85fa38
SalPGS <117068597+SalPGS@users.noreply.github.com>
2022-11-02 10:53:26 +1100

    Create losses.py

---
+++ nets/losses.py
@@ -0,0 +1,53 @@
+"""
+ Loss and weight modules of DGD-cGAN model
+   * Paper: 
+"""
+#Libraries
+import torch
+from torch import nn
+
+
+class GANLoss(nn.Module):
+    def __init__(self, real_label=1.0, fake_label=0.0):
+        super().__init__()
+        self.register_buffer('real_label', torch.tensor(real_label))
+        self.register_buffer('fake_label', torch.tensor(fake_label))
+        self.loss = nn.BCEWithLogitsLoss()
+
+    def get_labels(self, preds, target_is_real):
+        if target_is_real:
+            labels = self.real_label
+        else:
+            labels = self.fake_label
+        return labels.expand_as(preds)
+    
+    def __call__(self, preds, target_is_real):
+        labels = self.get_labels(preds, target_is_real)
+        loss = self.loss(preds, labels)
+        return loss
+
+#Weights
+
+def weights(net, init='weight', gain=0.02):
+    # Normal distribution to initalize weights(mean, std)
+    def init_func(m):
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and 'Conv' in classname:
+            if init == 'norm':
+                nn.init.normal_(m.weight.data, mean=0.0, std=gain)
+            elif init == 'xavier':
+                nn.init.xavier_normal_(m.weight.data, gain=gain)
+            
+            if hasattr(m, 'bias') and m.bias is not None:
+                nn.init.constant_(m.bias.data, 0.0)
+        elif 'BatchNorm2d' in classname:
+            nn.init.normal_(m.weight.data, 1.0, gain)
+            nn.init.constant_(m.bias.data, 0.0)
+            
+    net.apply(init_func)
+    return net
+
+def init_weights(model, device):
+    model = model.to(device)
+    model = weights(model)
+    return model
