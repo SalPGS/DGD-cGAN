badb28657208563166e32a82e211c084a488cba3
SalPGS <117068597+SalPGS@users.noreply.github.com>
2022-11-03 16:34:30 +1100

    Update utils.py

--- nets/utils.py
+++ nets/utils.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import torch
 
 
-
 #Loss meter
 class AverageMeter:
     def __init__(self):
@@ -18,23 +17,23 @@ class AverageMeter:
         self.avg = self.sum / self.count
 
 def create_loss_meters():
-    loss_D_fake = AverageMeter()
-    loss_D_real = AverageMeter()
-    loss_D = AverageMeter()
-    loss_G_GAN = AverageMeter()
-    loss_G_L1 = AverageMeter()
-    loss_G = AverageMeter()
-    loss_G_L2  = AverageMeter()
+    D_fake_Loss = AverageMeter()
+    D_real_Loss = AverageMeter()
+    D_Loss = AverageMeter()
+    G_Loss = AverageMeter()
+    G1_L1_Loss = AverageMeter()
+    loss_Gens = AverageMeter()
+    G2_L2_Loss  = AverageMeter()
 
     
     
-    return {'loss_D_fake': loss_D_fake,
-            'loss_D_real': loss_D_real,
-            'loss_D': loss_D,
-            'loss_G_GAN': loss_G_GAN,
-            'loss_G_L1': loss_G_L1,
-            'loss_G': loss_G,
-            'loss_G_L2':loss_G_L2
+    return {'D_fake_Loss': D_fake_Loss,
+            'D_real_Loss': D_real_Loss,
+            'D_Loss': D_Loss,
+            'G_Loss': G_Loss,
+            'G1_L1_Loss': G1_L1_Loss,
+            'loss_Gens': loss_Gens,
+            'G2_L2_Loss':G2_L2_Loss
             }
 
 
@@ -44,22 +43,24 @@ def update_losses(model, loss_meter_dict, count):
         loss = getattr(model, loss_name)
         loss_meter.update(loss.item(), count=count) 
 
+
+
 # Display images    
 def grid_output(model, data):
-    model.net_G.eval()
+    model.G1.eval()
     with torch.no_grad():
         model.set_input(data)
         model.forward()
-    model.net_G.train()
-    fake_color = model.fake_color.detach()
-    real_color = model.Gt
+    model.G1.train()
+    fake_dewater = model.fake_dewater.detach()
+    groun_truth = model.Gt
     Uw = model.Uw.cpu().numpy()
     Uw = np.transpose(Uw,(0,2,3,1))
 
-    fake_imgs=fake_color.cpu().numpy()
+    fake_imgs= fake_dewater.cpu().numpy()
     fake_imgs = np.transpose(fake_imgs,(0,2,3,1))
 
-    real_imgs=real_color.cpu().numpy()
+    real_imgs= groun_truth.cpu().numpy()
     real_imgs = np.transpose(real_imgs,(0,2,3,1))
 
     fig = plt.figure(figsize=(15, 8))
